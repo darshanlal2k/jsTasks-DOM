6f85dc673bcedb7b83bbcb5465efa53a3e863398
darshanlal2k <darshanlal2k@gmail.com>
2023-08-18 08:50:19 +0530

    script.js has changed some names and changed variable name

--- form_validation/js/script.js
+++ form_validation/js/script.js
@@ -1,211 +1,254 @@
-var user_name = document.getElementById("username");
-
-user_name.addEventListener("keypress", username);
-// var ab;
-
-function username(event) {
-    if (((event.keyCode > 64) && (event.keyCode <= 90)) || ((event.keyCode > 96) && (event.keyCode <= 122))) {
-        return true;
-    } else {
-        event.preventDefault();
-        alert("Enter Only Alphabets");
-    }
-};
-
+var username = document.getElementById("username");
 var age = document.getElementById("age");
-age.addEventListener("keypress", user_age);
-
-function user_age(event) {
-    if ((((event.keyCode > 48) && (event.keyCode <= 57))) && (age.value.length < 2)) {
-        return true;
-    } else {
-        event.preventDefault();
-        alert("Enter Only Numbers");
-    }
-};
-
 var gender_value;
-var gender = document.getElementsByName("gender").forEach(function(e) {
+var gender;
+var mobile = document.getElementById("mobile_number");
+var aadhar = document.getElementById("aadhar_number");
+var email = document.getElementById("email");
+var interestArea = document.getElementsByName("interestArea");
+var state = document.getElementById("state");
+var country = document.getElementById("Country");
+var interestValues = [];
+let interestChecked;
+var stateSelected;
+var stateOption;
+var stateName;
+var district = document.getElementById("district");
+var districtSelected;
+var districtOption;
+var districtName;
+var address = document.getElementById("address");
+var comments = document.getElementById("comments");
+var comments_value;
+var reset = document.getElementById("reset_btn");
+var submit = document.getElementById("submit_btn");
+var emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+var form = document.getElementById("myForm");
+
+username.addEventListener("keypress", userName);
+age.addEventListener("keypress", userAge);
+mobile.addEventListener("keypress", userMobile);
+aadhar.addEventListener("keypress", userAadhar);
+email.addEventListener("focusout", userEmail);
+country.addEventListener("keyup", userCountry);
+comments.addEventListener("change", userComments);
+address.addEventListener("change", userAddress);
+reset.addEventListener("click", resetAll);
+submit.addEventListener("click", submitAll);
+
+
+
+gender = document.getElementsByName("gender").forEach(function(e) {
     e.addEventListener("change", function() {
         gender_value = e.value;
         // alert("You have selected" + " " + gender_value + " " + "Gender");
     });
 });
 
-
-var mobile = document.getElementById("mobile_number");
-mobile.addEventListener("keypress", user_mobile);
-
-function user_mobile(event) {
-    if (((event.keyCode > 47) && (event.keyCode <= 57)) && (mobile.value.length < 10)) {
-        return true;
-    } else {
-        event.preventDefault();
-        alert("Enter Only Numbers");
-    }
-};
-
-var aadhar = document.getElementById("aadhar_number");
-aadhar.addEventListener("keypress", user_aadhar);
-
-function user_aadhar(event) {
-    if (((event.keyCode > 47) && (event.keyCode <= 57)) && (aadhar.value.length < 12)) {
-        return true;
-    } else {
-        event.preventDefault();
-        alert("Enter Only Numbers");
-    }
-};
-
-var email = document.getElementById("email");
-email.addEventListener("focusout", user_email);
-
-function user_email() {
-    const emailField = email.value;
-    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if (emailField.match(mailformat)) {
-        //  alert("Valid email address!");
-        return true;
-    } else {
-        alert("You have entered an invalid email address!");
-    }
-};
-
-var interest = document.getElementsByName("interest");
-for (i = 0; i < interest.length; i++) {
-    interest[i].addEventListener("change", user_interest);
-};
-
-var interest_values;
-let interest_checked;
-
-function user_interest() {
-    interest_checked = document.querySelectorAll('input[name="interest"]:checked');
-    interest_values = [];
-    interest_checked.forEach((interest) => {
-
-        interest_values.push(interest.value);
-    });
-    // alert(interest_values.length);
-};
-
-var state = document.getElementById("state");
-state.addEventListener("change", function() { state_dropdown(this) });
-// state.addEventListener("change", dropdown);
-var state_selected;
-var state_option;
-var state_name;
-
-function state_dropdown(event) {
-    // console.log(event.target);
-    state_selected = event.selectedIndex;
-    state_option = event.options;
-    state_name = state_option[state_selected];
-    // alert("Index: " + state_option[state_selected].index + " is " + state_option[state_selected].text);
-    // alert(state_name.value);
-};
-
-var district = document.getElementById("district");
-district.addEventListener("change", function() { district_dropdown(this) });
-
-var district_selected;
-var district_option;
-
-function district_dropdown(event) {
-    district_selected = event.selectedIndex;
-    district_option = event.options;
-    district_name = district_option[district_selected];
-    //  alert(district_name.value);
-    // alert("Index: " + district_option[district_selected].index + " is " + district_option[district_selected].text);
+for (i = 0; i < interestArea.length; i++) {
+    interestArea[i].addEventListener("change", userInterestArea);
 }
 
-var country = document.getElementById("Country");
-country.addEventListener("keypress", user_country);
+state.addEventListener("change", function() {
+    chooseState(this);
+});
 
-function user_country(event) {
-    if (((event.keyCode > 64) && (event.keyCode <= 90)) || ((event.keyCode > 96) && (event.keyCode <= 122))) {
+district.addEventListener("change", function() {
+    chooseDistrict(this);
+});
+
+function userCountry(event) {
+    if (
+        (event.keyCode > 64 && event.keyCode <= 90) ||
+        (event.keyCode > 96 && event.keyCode <= 122)
+
+    ) {
+        console.log(event.target.value);
         return true;
     } else {
         event.preventDefault();
         alert("Enter Only Alphabets");
     }
-};
+}
 
-var address = document.getElementById("address");
-console.log(address);
-address.addEventListener("change", user_address);
+function userAddress(event) {
+    console.log(event.target.value);
+}
 
-function user_address() {
-    // alert(address.value);
-};
+function userComments(event) {
+    console.log(event.target.value);
+}
 
-var comments = document.getElementById("comments");
-comments.addEventListener("change", user_comments);
+function resetAll() {
+    form.reset();
+}
 
-var comments_value;
 
-function user_comments(event) {
-    // alert(event.value);
-    // alert(comments_value = event.value);
-};
 
-var reset = document.getElementById("reset_btn");
-reset.addEventListener("click", reset_all);
-
-function reset_all() {
-    document.getElementById("myForm").reset();
-};
-
-var submit = document.getElementById("submit_btn");
-submit.addEventListener("click", submit_all);
-
-function submit_all() {
-    //console.log(document.body.form);
+function submitAll(event) {
+    console.log(event.target.value);
     // var total_form = document.getElementById("myform");
-    //console.log(total_form.username.value);
+    console.log(form.username.value);
     // console.log(x);
-    if (user_name.value == '') {
+    if (username.value == "") {
         alert("please enter your Username");
-    } else if (age.value == '') {
+    } else if (age.value == "") {
         alert("please enter your Age");
     }
     //console.log(x);
-    else if ((gender_value == '') || (gender_value == undefined)) {
+    else if (gender_value == "" || gender_value == undefined) {
         alert("please enter your Gender");
-    } else if (mobile.value == '') {
+    } else if (mobile.value == "") {
         alert("please enter your Mobile");
-    } else if (aadhar.value == '') {
+    } else if (aadhar.value == "") {
         alert("please enter your Aadhar");
-    } else if (email.value == '') {
+    } else if (email.value == "") {
         alert("please enter your Email");
-    } else if ((interest_values == '') || (interest_values == undefined)) {
+    } else if (interestValues == "" || interestValues == undefined) {
         alert("please enter your Interest");
-    } else if ((state_name.value == '') || (state_name.value == "none")) {
+    } else if (stateName.value == "" || stateName.value == "none") {
         alert("please enter your State");
-    } else if ((district_name.value == '') || (district_name.value == "none")) {
+    } else if (districtName.value == "" || districtName.value == "none") {
         alert("please enter your District");
-    } else if (country.value == '') {
+    } else if (country.value == "") {
         alert("please enter your Country");
-    } else if (address.value == '') {
+    } else if (address.value == "") {
         alert("please enter your Address");
-    } else if (comments.value == '') {
+    } else if (comments.value == "") {
         alert("please enter your Comments");
     } else {
         //  alert("ok");
 
-
         alert(
-            "Name is  :  " + " " + user_name.value + "\n" +
-            "Age is :  " + " " + age.value + "\n" +
-            "Gender is :  " + " " + gender_value + "\n" +
-            "Mobile Number is : " + " " + mobile.value + "\n" +
-            "Aadhar Number is : " + " " + aadhar.value + "\n" +
-            "Email ID is : " + " " + email.value + "\n" +
-            "Interested Area is : " + " " + interest_values + "\n" +
-            "State is : " + " " + state_name.value + "\n" +
-            "District is : " + " " + district_name.value + "\n" +
-            "Country is : " + " " + country.value + "\n" +
-            "Address is : " + " " + address.value + "\n" +
-            "Comments is : " + " " + comments.value + "\n");
+            "Name is  :  " +
+            " " +
+            username.value +
+            "\n" +
+            "Age is :  " +
+            " " +
+            age.value +
+            "\n" +
+            "Gender is :  " +
+            " " +
+            gender_value +
+            "\n" +
+            "Mobile Number is : " +
+            " " +
+            mobile.value +
+            "\n" +
+            "Aadhar Number is : " +
+            " " +
+            aadhar.value +
+            "\n" +
+            "Email ID is : " +
+            " " +
+            email.value +
+            "\n" +
+            "Interested Area is : " +
+            " " +
+            interestValues +
+            "\n" +
+            "State is : " +
+            " " +
+            stateName.value +
+            "\n" +
+            "District is : " +
+            " " +
+            districtName.value +
+            "\n" +
+            "Country is : " +
+            " " +
+            country.value +
+            "\n" +
+            "Address is : " +
+            " " +
+            address.value +
+            "\n" +
+            "Comments is : " +
+            " " +
+            comments.value +
+            "\n"
+        );
     }
-};
+}
+
+function userName(event) {
+    if (
+        (event.keyCode > 64 && event.keyCode <= 90) ||
+        (event.keyCode > 96 && event.keyCode <= 122)
+    ) {
+        return true;
+    } else {
+        event.preventDefault();
+        alert("Enter Only Alphabets");
+    }
+}
+
+function userAge(event) {
+    if (event.keyCode > 48 && event.keyCode <= 57 && age.value.length < 2) {
+        return true;
+    } else {
+        event.preventDefault();
+        alert("Enter Only Numbers");
+    }
+}
+
+function userMobile(event) {
+    if (
+        event.keyCode > 47 &&
+        event.keyCode <= 57 &&
+        event.target.value.length < 10
+    ) {
+        return true;
+    } else {
+        event.preventDefault();
+        alert("Enter Only Numbers");
+    }
+}
+
+function userAadhar(event) {
+    if (
+        event.keyCode > 47 &&
+        event.keyCode <= 57 &&
+        event.target.value.length < 12
+    ) {
+        return true;
+    } else {
+        event.preventDefault();
+        alert("Enter Only Numbers");
+    }
+}
+
+function userEmail(event) {
+    if (event.target.value.match(emailFormat)) {
+        return true;
+    } else {
+        alert("You have entered an invalid email address!");
+    }
+}
+
+function userInterestArea() {
+    interestChecked = document.querySelectorAll(
+        'input[name="interestArea"]:checked'
+    );
+    interestValues = [];
+    interestChecked.forEach((interestArea) => {
+        interestValues.push(interestArea.value);
+    });
+    console.log(interestValues);
+}
+
+function chooseState(event) {
+    stateSelected = event.selectedIndex;
+    stateOption = event.options;
+    stateName = stateOption[stateSelected];
+}
+
+function chooseDistrict(event) {
+    districtSelected = event.selectedIndex;
+    districtOption = event.options;
+    districtName = districtOption[districtSelected];
+    console.log(districtName.value);
+    console.log(districtOption[districtSelected].index);
+    console.log(districtOption[districtSelected].text);
+}
